e4ff37c83be2eadd8562a10a8988f32b46a93d59
Zero_Chaos <zerochaos-@users.noreply.github.com>
2010-10-08 17:24:38 +0000

    sys-power/acpid: added our own acpid so I could influence the processor on AC vs on BATT this action should completely replace cpufreqd and waste less CPU

---
+++ sys-power/acpid/files/acpid-pentoo-1.0.6-default.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# /etc/acpi/default.sh
+# Default acpi script that takes an entry for all actions
+
+set $*
+
+group=${1%%/*}
+action=${1#*/}
+device=$2
+id=$3
+value=$4
+
+log_unhandled() {
+	logger "ACPI event unhandled: $*"
+}
+
+case "$group" in
+	button)
+		case "$action" in
+			power)
+				/sbin/init 0
+				;;
+
+			# if your laptop doesnt turn on/off the display via hardware
+			# switch and instead just generates an acpi event, you can force
+			# X to turn off the display via dpms.  note you will have to run
+			# 'xhost +local:0' so root can access the X DISPLAY.
+			#lid)
+			#	xset dpms force off
+			#	;;
+
+			*)	log_unhandled $* ;;
+		esac
+		;;
+
+	ac_adapter)
+		case "$value" in
+			# Add code here to handle when the system is unplugged
+			# (maybe change cpu scaling to powersave mode).  For
+			# multicore systems, make sure you set powersave mode
+			# for each core!
+			*0)
+				for CPU in `ls  /sys/devices/system/cpu/|grep -E "cpu[0-9]+"|cut -d"u" -f2`; do cpufreq-set -r -c $CPU -g ondemand; done
+				;;
+
+			# Add code here to handle when the system is plugged in
+			# (maybe change cpu scaling to performance mode).  For
+			# multicore systems, make sure you set performance mode
+			# for each core!
+			*1)
+				for CPU in `ls  /sys/devices/system/cpu/|grep -E "cpu[0-9]+"|cut -d"u" -f2`; do cpufreq-set -r -c $CPU -g performance; done
+				;;
+
+			*)	log_unhandled $* ;;
+		esac
+		;;
+
+	*)	log_unhandled $* ;;
+esac
