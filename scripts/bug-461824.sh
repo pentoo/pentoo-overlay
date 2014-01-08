55b0ebf6204ab52c61d24b06b133b631ba32a1bd
Zero_Chaos <zerochaos-@users.noreply.github.com>
2014-01-08 18:23:50 +0000

    profile: new scripts dir to house things which may be needed for everyone

---
+++ scripts/bug-461824.sh
@@ -0,0 +1,13 @@
+#work around for detecting and fixing bug #461824
+grep -r _portage_reinstall_ /etc {/usr,}/{*bin,lib*} | grep -v doebuild > /tmp/urfuct.txt
+if [ -n "$(cat /tmp/urfuct.txt)" ]; then
+	for badhit in "$(cat /tmp/urfuct.txt)" ; do
+		echo ${badhit} | cut -d":" -f1 >> /tmp/badfiles.txt
+	done
+	for badfile in $(cat /tmp/badfiles.txt); do
+		qfile -C ${badfile} | cut -d' ' -f1 >> /tmp/badpkg_us.txt
+	done
+	cat /tmp/badpkg_us.txt | sort -u > /tmp/badpkg.txt
+	emerge -1 --buildpkg=y --nodeps $(cat /tmp/badpkg.txt) || /bin/bash
+	rm -f /tmp/urfuct.txt /tmp/badfiles.txt /tmp/badpkg_us.txt /tmp/badpkg.txt
+fi
