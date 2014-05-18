2bc3fc26ed6b8bfe7c39937ab72a7c93787d9895
Zero_Chaos <zerochaos-@users.noreply.github.com>
2014-05-18 01:54:05 +0000

    livecd updates and profile updates.  now pentoo-updater.sh is available to users for TESTING

---
+++ scripts/pentoo-updater.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+source /etc/profile
+env-update
+
+if [ -f /.catalyst_lock ]; then
+	if [ -f /tmp/envscript ]; then
+		source /tmp/envscript
+		emerge --info > /var/log/portage/emerge-info-$(date "+%Y%m%d").txt
+	fi
+fi
+
+safe_exit() {
+	#I want a shell when I'm in catalyst but just an exit on failure for users
+	if [ -f /.catalyst_lock ]; then
+		/bin/bash
+	else
+		exit
+	fi
+}
+
+emerge --update --newuse --oneshot portage || safe_exit
+
+#first we set the python interpreters to match PYTHON_TARGETS
+eselect python set --python2 $(emerge --info | grep ^PYTHON_TARGETS | cut -d\" -f2 | cut -d" " -f 1 |sed 's#_#.#') || safe_exit
+eselect python set --python3 $(emerge --info | grep ^PYTHON_TARGETS | cut -d\" -f2 | cut -d" " -f 2 |sed 's#_#.#') || safe_exit
+python2.7 -c "from _multiprocessing import SemLock" || emerge -1 --buildpkg=y python:2.7
+python3.3 -c "from _multiprocessing import SemLock" || emerge -1 --buildpkg=y python:3.3
+python-updater -- --buildpkg=y --rebuild-exclude sys-devel/gdb --exclude sys-devel/gdb || safe_exit
+
+perl-cleaner --ph-clean --modules -- --buildpkg=y || safe_exit
+
+emerge --deep --update --newuse -kb @world || safe_exit
+
+#if we are in catalyst, update the extra binpkgs
+if [ -f /.catalyst_lock ]; then
+	#add kde and mate use flags
+	echo "pentoo/pentoo kde mate" >> /etc/portage/package.use
+	emerge --onlydeps --oneshot --deep --update --newuse pentoo/pentoo || safe_exit
+	etc-update --automode -5 || safe_exit
+fi
+
+emerge @preserved-rebuild --buildpkg=y || safe_exit
+smart-live-rebuild 2>&1 || safe_exit
+revdep-rebuild.py -i --no-pretend -- --rebuild-exclude dev-java/swt --exclude dev-java/swt --buildpkg=y || safe_exit
+emerge --deep --update --newuse -kb @world || safe_exit
+etc-update --automode -5 || safe_exit
+#we need to do the clean BEFORE we drop the extra flags otherwise all the packages we just built are removed
+emerge --depclean || safe_exit
+
+if [ -f /.catalyst_lock ]; then
+	eclean-pkg || safe_exit
+	emaint binhost || safe_exit
+	#remove kde/mate use flags
+	rm /etc/portage/package.use
+fi
+
+/usr/local/portage/scripts/bug-461824.sh
