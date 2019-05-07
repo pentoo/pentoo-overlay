e5131a969efafe95ba9a1e056b0d8b1eaecf9fdf
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2019-05-07 15:09:35 -0400

    pentoo-updater: back to detecting issues with repo management

--- scripts/pentoo-updater.sh
+++ scripts/pentoo-updater.sh
@@ -338,9 +338,9 @@ if [ -n "${clst_target}" ]; then
   fi
   emerge @changed-deps || safe_exit
   etc-update --automode -5 || safe_exit
-  eclean-pkg -d -t 3m || true
-  fixpackages || true
-  emaint --fix binhost || true
+  eclean-pkg -d -t 3m || safe_exit
+  fixpackages || safe_exit
+  emaint --fix binhost || safe_exit
   #remove kde/mate use flags
   rm /etc/portage/package.use
 fi
