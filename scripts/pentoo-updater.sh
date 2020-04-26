c55cf6ac64298671b1150c5d584f8d4df1a9a4e1
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2020-04-26 11:20:26 -0400

    pentoo-updater: hopefully binpkg-multi-instance works now

--- scripts/pentoo-updater.sh
+++ scripts/pentoo-updater.sh
@@ -427,8 +427,6 @@ if [ -n "${clst_target}" ]; then
   echo 'pentoo/pentoo-cracking amdopencl intel-opencl' >> /etc/portage/profile/package.use
   #add in pentoo-extra to build more binpkgs
   echo 'USE="pentoo-extra"' >> /etc/portage/profile/make.defaults
-  #so, we don't want binpkgs of mesa for non-defaults, but we have to built it to build the rest of kde
-  emerge --buildpkg=n media-libs/mesa --oneshot || safe_exit
   emerge --buildpkg @changed-deps || safe_exit
   emerge --buildpkg --usepkg --onlydeps --oneshot --deep --update --newuse --changed-deps --newrepo pentoo/pentoo || safe_exit
   etc-update --automode -5 || safe_exit
